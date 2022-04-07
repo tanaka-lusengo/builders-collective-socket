236b9ec94d46791067eac6dba808de177dc74c12
Tanaka Lusengo <79418686+tanaka-lusengo@users.noreply.github.com>
2022-04-07 10:39:49 +0100

    new socket branch

---
+++ app.js
@@ -0,0 +1,66 @@
+// initial library installation set up
+const express = require("express");
+const app = express();
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use(cors());
+
+// messaging/My Network feature
+//--------------------------------------------------
+
+// establish connection to new instance "server"
+const server = http.createServer(app);
+
+// cors management safe guarding
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+// listening for event with "connection" id
+io.on("connection", (socket) => {
+  //welcomes current user
+  socket.emit("message", "Welcome to the Builders' Collective Chat Stream");
+
+  // broadcast to other users when a new user connects
+  socket.broadcast.emit("message", "A user has joined the chat");
+
+  // upon joining a room
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  // Listen for sendMessage() function for message sent from chat room
+  socket.on("send_message", (messageData) => {
+    socket.to(messageData.room).emit("receive_message", messageData);
+  });
+
+  // lets all users know when a user disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left the chat");
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.get("/", (_req, res) => {
+  res.send("Welcome to the Builders' Collective Socket Server!");
+});
+
+server.listen(PORT, () => {
+  console.log(
+    `Builders' Collective Network Chat listening on http://localhost:${PORT}`
+  );
+});
